825413aa2078d2f2b5ba8d3affdcb1c1c82589f9
habato <kobatochan22@yahoo.co.jp>
2021-07-19 16:09:19 +0900

    各カテゴリーの選択項目を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,22 @@ class Item < ApplicationRecord
 
   validates :product_name,    presence: true
   validates :description,     presence: true
-  validates :category_id,     presence: true
-  validates :status_id,       presence: true
-  validates :burden_id,       presence: true
-  validates :prefectures_id,  presence: true
-  validates :shipment_day_id, presence: true
+  validates :category_id,     numericality: { other_than: 1, message: "can't be blank"} 
+  validates :status_id,       numericality: { other_than: 1, message: "can't be blank"} 
+  validates :burden_id,       numericality: { other_than: 1, message: "can't be blank"} 
+  validates :prefectures_id,  numericality: { other_than: 0, message: "can't be blank"} 
+  validates :shipment_day_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :price,           presence: true
   validates :image,           presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :burden
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefectures
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipment_day
 end
