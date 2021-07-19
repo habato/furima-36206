cbb8f8d66c887774a1b56e4d40af7627c4f8e5d6
habato <kobatochan22@yahoo.co.jp>
2021-07-19 09:29:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :product_name,    presence: true
+  validates :description,     presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :burden_id,       presence: true
+  validates :prefectures_id,  presence: true
+  validates :shipment_day_id, presence: true
+  validates :price,           presence: true
+  validates :image,           presence: true
 end
