43d920aea2d2124a44789346207b8e8ff4ca4178
habato <kobatochan22@yahoo.co.jp>
2021-07-19 19:38:55 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image,           presence: true
   validates :product_name,    presence: true
   validates :description,     presence: true
   validates :category_id,     numericality: { other_than: 1, message: "can't be blank"} 
@@ -10,9 +11,9 @@ class Item < ApplicationRecord
   validates :burden_id,       numericality: { other_than: 1, message: "can't be blank"} 
   validates :prefectures_id,  numericality: { other_than: 0, message: "can't be blank"} 
   validates :shipment_day_id, numericality: { other_than: 1, message: "can't be blank"} 
-  validates :price,           presence: true
-  validates :image,           presence: true
 
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'}, inclusion: { in: 300..9_999_999, message: "is out of setting range" }
+  validates :price, numericality: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
