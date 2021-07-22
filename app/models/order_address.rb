e0bca401de856e1bc9316400e09fc8287c296a84
habato <kobatochan22@yahoo.co.jp>
2021-07-22 14:45:16 +0900

    バリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :item_id
@@ -9,7 +9,7 @@ class OrderAddress
     validates :municipality
     validates :address
     validates :phone_number, format: {with:/\A[0-9]{10,11}\z/, message: "is invalid. Input only number"}
-    validates :order_id
+
   end
   validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
 
