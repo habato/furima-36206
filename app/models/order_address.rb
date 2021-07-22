72d19eb2bb8348daadaf26dcca79a01f3abcc714
habato <kobatochan22@yahoo.co.jp>
2021-07-22 14:21:54 +0900

    エラーメッセージが表示されるよう設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,10 +5,10 @@ class OrderAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipality
     validates :address
-    validates :phone_number, format: {with:/\A[0-9]{10,11}\z/}
+    validates :phone_number, format: {with:/\A[0-9]{10,11}\z/, message: "is invalid. Input only number"}
     validates :order_id
   end
   validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
