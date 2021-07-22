1b46b87aea327626ad69493e6736fe4e83c56e3a
habato <kobatochan22@yahoo.co.jp>
2021-07-22 22:02:05 +0900

    トークンに対するバリデーションを設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,15 +2,15 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
   attr_accessor :token
-  
+
   with_options presence: true do
+    validates :token
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :municipality
     validates :address
     validates :phone_number, format: {with:/\A[0-9]{10,11}\z/, message: "is invalid. Input only number"}
-
   end
   validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"}
 
