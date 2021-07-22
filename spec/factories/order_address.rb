a09ce4ebb7a37acbe57b561a15e1250484ceaaa5
habato <kobatochan22@yahoo.co.jp>
2021-07-22 22:07:37 +0900

    トークンに関連するテストコードを追加

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :order_address do
     postal_code    { '123-4567' }
     prefectures_id { 14 }
-    municipality   { '東京都' }
+    municipality   { '東京' }
     address        { '1-1' }
     building_name  { '東京ハイツ' }
     phone_number   { '09012345678' }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
