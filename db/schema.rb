2b0db24faec4accc78120d6c3563da6647e83ecb
habato <kobatochan22@yahoo.co.jp>
2021-07-23 12:30:19 +0900

    不要なカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_000022) do
+ActiveRecord::Schema.define(version: 2021_07_22_143717) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,8 +58,6 @@ ActiveRecord::Schema.define(version: 2021_07_23_000022) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "order_id"
-    t.index ["order_id"], name: "index_items_on_order_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
