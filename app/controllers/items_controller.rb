f2a511d10c018bcc097bb74a245f825e33230051
habato <kobatochan22@yahoo.co.jp>
2021-08-18 21:21:56 +0900

    コメント一覧の表示を変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
 
   def show
     @comment = Comment.new
-    @comments = @item.comments.includes(:user).order(created_at: :desc)
+    @comments = @item.comments.includes(:user)
   end
 
   def edit
